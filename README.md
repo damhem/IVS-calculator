fef94d7ec5358d89e2daebaca9b3e1f5ead5ccd8
Dalibor Králik <dadokralik@gmail.com>
2021-03-22 16:35:26 +0100

    MathlibcalcProposal and README update
    Adding mathlibcalcProposal.py and updating README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-IVS-calculator
+Prostredi
+---------
+Ubuntu 64bit
+
+Autori
+------
+
+DDPPy
+- xkrali20 Dalibor Králik 
+- xsehno01 Patrik Sehnoutek
+- xcecha06 Daniel Čechák
+
+Licence
+-------
+
+Tento program je poskytovaný s licenciou GPL v3.0. 
---
+++ src/mathlibcalcProposal.py
@@ -0,0 +1,103 @@
+# mathlibcalc.py
+# Project IVS 2
+# Author: Dalibor Králik, xkrali20
+# Date: 2021-03-22
+
+#Add file doxygen
+
+
+##
+# @brief Class Calclib for the Calculator, contains math functions
+#
+class Calclib:
+
+    ##
+    # @brief Function for the sum of two numbers
+    #
+    # @param num1 First number
+    # @param num2 Second number
+    #
+    # @return Return sum of two numbers
+    @staticmethod
+    def plus(num1,num2):
+        pass
+
+    ##
+    # @brief Function for the difference of two numbers
+    #
+    # @param num1 First number
+    # @param num2 Second number
+    #
+    # @return Difference of two numbers
+    @staticmethod
+    def minus(num1,num2):
+        pass
+
+    ##
+    # @brief Function for multiplying two numbers
+    #
+    # @param num1 First number
+    # @param num2 Second number
+    #
+    # @return Multiple of two numbers
+    @staticmethod
+    def multiply(num1,num2):
+        pass
+
+    ##
+    # @brief Function for dividing two numbers
+    #
+    # @param num1 First number
+    # @param num2 Second number
+    #
+    # @throw ZeroDivisionError If num2 is equal to 0
+    # @return Return division of two numbers
+    @staticmethod
+    def divide(num1,num2):
+        pass
+
+    ##
+    # @brief Function for absolute value
+    #
+    # @param num1  Number
+    #
+    # @return Absolute value of num1
+    @staticmethod
+    def operation_abs(num1):
+        pass
+
+    ##
+    # @brief Function for factorial
+    #
+    # @param num1 Number
+    #
+    # @throw ValueError If num1 is less than 0 or if num1 is not integer
+    # @return Factorial of num1
+    @staticmethod
+    def factorial(num1):
+        pass
+
+    ##
+    # @brief Function for exponentiation of a number
+    #
+    # @param num1 Number
+    # @param expo Exponent
+    #
+    # @return Exponentiate the number of the exponent
+    @staticmethod
+    def exponent(num1, expo):
+        pass
+
+    ##
+    # @brief Root function
+    #
+    # @param num1 Number
+    # @param expo Root number
+    #
+    # @throw ValueError If num1 is less then 0 and root number is even, or if root number is equal to 0
+    # @return Root of num1
+    @staticmethod
+    def root(num1, expo):
+        pass
+
+
